e134647a0179b91fef75a93b7e98f8cc2e9a38cf
bpandamao <58809691+bpandamao@users.noreply.github.com>
2021-06-27 23:33:07 +0800

    Update stick-breaking.r

--- stick-breaking.r
+++ stick-breaking.r
@@ -1,7 +1,7 @@
 # stick-breaking process
 # target:returns pi(weights) theta
 
-# n--iteration
+# n--k group
 # alpha the scale parameter of DP process
 # H-> gussian
 hfr=function(n)
@@ -14,7 +14,7 @@ hfr=function(n)
 # theta -> sampling from H
 
 # Example:
-# SBgenerate(10,3,hfr)
+# SBgenerate(n=10,alpha=3,H=hfr)
 #               [,1]      [,2]       [,3]      [,4]      [,5]       [,6]      [,7]        [,8]         [,9]     [,10]
 #   pi     0.01875315 0.1463867  0.1427548 0.2474484 0.1678510  0.0529528 0.1013868  0.04031811  0.009175294 0.0115453
 #   thetas 0.55602329 0.5705367 -1.3318886 1.2398157 0.8951297 -0.7017396 0.3008387 -1.25765785 -0.231572127 1.2880064
